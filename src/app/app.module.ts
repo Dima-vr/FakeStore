3a669bd124fadb3c194cfeebe1a57908e4cbaec1
Dima-vr <120222057+Dima-vr@users.noreply.github.com>
2023-06-10 14:15:35 +0300

    final project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -12,9 +13,12 @@ import { ProductListComponent } from './product-list/product-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: 'apiUrl', useValue: 'https://reqres.in/api' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,16 +8,34 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ProductListComponent implements OnInit {
   products: any[] = [];
+  filteredProducts: any[] = [];
 
 
   constructor(private http: HttpClient) {
     this.products = [];
+    this.filteredProducts = [];
   }
-  
 
   ngOnInit(): void {
     this.http.get<any[]>('https://fakestoreapi.com/products').subscribe(data => {
       this.products = data;
+      this.filteredProducts = data;
     });
+  }
+
+  filterProductsByCategory(category: string) {
+    this.filteredProducts = this.products.filter(product => product.category === category);
+  }
+
+  sortByPriceAscending() {
+    this.filteredProducts.sort((a, b) => a.price - b.price);
+  }
+
+  sortByPriceDescending() {
+    this.filteredProducts.sort((a, b) => b.price - a.price);
+  }
+
+  showAllProducts() {
+    this.filteredProducts = this.products;
   }
 }
